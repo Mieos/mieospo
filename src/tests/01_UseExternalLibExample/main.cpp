27f0795bf739cf7a416884d6ef25789b453fb7a0
Richard Modrzejewski <richard.modrzejewski@gmail.com>
2016-12-07 10:28:09 +0100

    Add Tests

---
+++ src/tests/01_UseExternalLibExample/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include"MieosProjectOrganiserExtLib.hpp"
+
+int main(int argc, char *argv[]){
+
+   std::cout << "MIEOSPO : Use of an external Library : TODO" << std::endl;
+   
+   return 0;
+}
